91c320e17bec499d00624d1e5bf0da714c3b2c00
SEH00N <06psh0501@gmail.com>
2025-09-28 00:13:39 +0900

    [etc]

--- packets/index.js
+++ packets/index.js
@@ -5,9 +5,3 @@ export { BufferReadHandle, BufferWriteHandle, getFlexiableUTF8Size, getFlexiable
 
 // Packets
 export { MessagePacket } from './messagepacket.js';
-
-// Managers
-import ResourceManager from '../../AlkkagiShared/Data/Resource/ResourceManager.js';
-
-// Init
-ResourceManager.load(true);
