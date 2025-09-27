61ece2be97a6de656c8a7daebd655a9cf6274bea
SEH00N <06psh0501@gmail.com>
2025-09-28 01:23:38 +0900

    [Packet] PacketFactory

--- packets/index.js
+++ packets/index.js
@@ -2,6 +2,7 @@
 export { EPacketID } from './base/epacketid.js';
 export { Packet } from './base/packet.js';
 export { BufferReadHandle, BufferWriteHandle, getFlexiableUTF8Size, getFlexiableUTF16LESize } from './base/bufferhandle.js';
+export { PacketFactory } from './base/packetfactory.js';
 
 // Packets
 export { MessagePacket } from './messagepacket.js';
