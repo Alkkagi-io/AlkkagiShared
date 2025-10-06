89e37e469c42884937c59dbfee25a9fd4345051c
2m8 <doyun7731@gmail.com>
2025-10-07 02:31:16 +0900

    StatType enum 추가

--- Resource/ResourceStat.js
+++ Resource/ResourceStat.js
@@ -1,6 +1,15 @@
 import { parseToInteger, parseToFloat } from "./DataParseHelper.js";
 import { ResourceBase } from "./ResourceBase.js";
 
+const EStatType = {
+    ADD_WEIGHT_PER: 1,
+    ADD_MAX_HP_PER: 2,
+    AUTO_HEAL_PER_MIN: 3,
+    ATK_COOLDOWN_PER: 4,
+    ADD_MAX_CHARGE_LEN_PER: 5,
+    ADD_MOVE_SPEED_PER: 6,
+};
+
 class ResourceStat extends ResourceBase {
     constructor() {
         super();
@@ -27,4 +36,4 @@ class ResourceStat extends ResourceBase {
     }
 }
 
-export { ResourceStat };
+export { ResourceStat, EStatType };
