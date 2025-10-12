667f4552a68e2fab6d9dc13248ff346e2f68b96f
2m8 <doyun7731@gmail.com>
2025-10-12 16:16:58 +0900

    ResourceShopItem 유틸 함수들 추가

--- Resource/ResourceShopItem.js
+++ Resource/ResourceShopItem.js
@@ -1,4 +1,5 @@
-const { ResourceBase } = require("./ResourceBase.js");
+import { parseToInteger, parseToBoolean, parseToFloat } from "./DataParseHelper.js";
+import { ResourceBase } from "./ResourceBase.js";
 
 class ResourceShopItem extends ResourceBase {
     constructor() {
@@ -8,9 +9,28 @@ class ResourceShopItem extends ResourceBase {
     }
 
     init(id, dict) {
-        super(id, dict);
+        super.init(id, dict);
         this.price = this.getDictValueInt("Price");
         this.ability = this.getDictValue("Ability");
+    }
+
+    getAbilityValue(key, defaultValue = "") {
+        return this.ability[key];
+    }
+
+    getAbilityValueInt(key, defaultValue = 0) {
+        const n = parseToInteger(this.ability[key]);
+        return Number.isFinite(n) ? n : defaultValue;
+    }
+
+    getAbilityValueFloat(key, defaultValue = 0) {
+        const n = parseToFloat(this.ability[key]);
+        return Number.isFinite(n) ? n : defaultValue;
+    }
+
+    getAbilityValueBool(key, defaultValue = false) {
+        const b = parseToBoolean(this.ability[key]);
+        return (b === undefined || b === null) ? defaultValue : Boolean(b);
     }
 }
 
