c4bab3839df68a08784132226caf843cb4d58719
SEH00N <06psh0501@gmail.com>
2025-10-16 05:34:15 +0900

    [Entity] Character Level Up

---
+++ Resource/ResourceCharacterLevel.js
@@ -0,0 +1,20 @@
+import { StatConfig } from "../../src/Stat/StatConfig.js";
+import { parseToInteger, parseToFloat } from "./DataParseHelper.js";
+import { ResourceBase } from "./ResourceBase.js";
+
+class ResourceCharacterLevel extends ResourceBase {
+    constructor() {
+        super();
+        this.level = 0;
+        this.requiredXP = 0; // from prev level. !! NOT TOTAL XP !!
+    }
+
+    init(id, dict) {
+        super.init(id, dict);
+
+        this.level = this.getDictValueInt("Level", 0);
+        this.requiredXP = this.getDictValueInt("RequiredXP", 0);
+    }
+}
+
+export { ResourceCharacterLevel };
