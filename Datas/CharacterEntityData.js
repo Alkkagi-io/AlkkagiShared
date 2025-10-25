12a4437c59a45fb9f2ce1d42802a424b9cf5396c
2m8 <doyun7731@gmail.com>
2025-10-25 23:14:06 +0900

    CharacterEntityData에 name 정보 추가

--- Datas/CharacterEntityData.js
+++ Datas/CharacterEntityData.js
@@ -3,19 +3,23 @@ import { EntityData } from './index.js';
 class CharacterEntityData extends EntityData {
     constructor(character) {
         super(character);
+        this.name = character.nickname;
     }
 
     getFlexibleSize() {
         let size = super.getFlexibleSize();
+        size += this.getFlexiableUTF8Size(this.name) // name
         return size;
     }
 
     onSerialize(writeHandle) {
         super.onSerialize(writeHandle);
+        writeHandle.writeStringUTF8(this.name);
     }
 
     onDeserialize(readHandle) {
         super.onDeserialize(readHandle);
+        this.name = readHandle.readStringUTF8();
     }
 }
 
