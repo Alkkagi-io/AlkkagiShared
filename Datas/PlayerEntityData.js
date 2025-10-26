ece03eb009a79fcb9014b5fbf93c4ae73455231b
SEH00N <06psh0501@gmail.com>
2025-10-26 22:26:34 +0900

    [Entity] Player Level Up Packet

--- Datas/PlayerEntityData.js
+++ Datas/PlayerEntityData.js
@@ -5,14 +5,14 @@ class PlayerEntityData extends CharacterEntityData {
     constructor(player) {
         super(player);
         this.entityType = EEntityType.Player;
-        this.level = player?.levelComponent?.level ?? 0;
+        // this.level = player?.levelComponent?.level ?? 0;
         this.exp = player?.levelComponent?.xpAmount ?? 0;
         this.score = player?.score ?? 0;
     }
 
     getFlexibleSize() {
         let size = super.getFlexibleSize();
-        size += 4; // level
+        // size += 4; // level
         size += 4; // exp
         size += 4; // score
         return size;
@@ -20,14 +20,14 @@ class PlayerEntityData extends CharacterEntityData {
 
     onSerialize(writeHandle) {
         super.onSerialize(writeHandle);
-        writeHandle.writeUint16(this.level);
+        // writeHandle.writeUint16(this.level);
         writeHandle.writeUint16(this.exp);
         writeHandle.writeUint16(this.score);
     }
 
     onDeserialize(readHandle) {
         super.onDeserialize(readHandle);
-        this.level = readHandle.readUint16();
+        // this.level = readHandle.readUint16();
         this.exp = readHandle.readUint16();
         this.score = readHandle.readUint16();
     }
