d88e37c82dfd7dd9a5a2f7fa05eb8c78826fd968
2m8 <doyun7731@gmail.com>
2025-10-30 00:26:21 +0900

    playerEntityData 정보 추가

--- Datas/PlayerEntityData.js
+++ Datas/PlayerEntityData.js
@@ -8,6 +8,13 @@ class PlayerEntityData extends CharacterEntityData {
         // this.level = player?.levelComponent?.level ?? 0;
         this.exp = player?.levelComponent?.xpAmount ?? 0;
         this.score = player?.score ?? 0;
+
+        const attackComponent = player?.attackComponent ?? undefined;
+        const remainAtkCoolRatio = attackComponent?.getRemainAttackCooltimePer() * 100 ?? 0;
+        this.remainAtkCoolPer = Math.round(remainAtkCoolRatio);
+
+        const chargingRatio = attackComponent?.getChargingPer() * 100 ?? 0;
+        this.chargingPer = Math.round(chargingRatio);
     }
 
     getFlexibleSize() {
@@ -15,6 +22,8 @@ class PlayerEntityData extends CharacterEntityData {
         // size += 4; // level
         size += 4; // exp
         size += 4; // score
+        size += 4; // remainAtkCoolPer
+        size += 4; // chargingPer
         return size;
     }
 
@@ -23,6 +32,8 @@ class PlayerEntityData extends CharacterEntityData {
         // writeHandle.writeUint16(this.level);
         writeHandle.writeUint16(this.exp);
         writeHandle.writeUint16(this.score);
+        writeHandle.writeUint16(this.remainAtkCoolPer);
+        writeHandle.writeUint16(this.chargingPer);
     }
 
     onDeserialize(readHandle) {
@@ -30,6 +41,8 @@ class PlayerEntityData extends CharacterEntityData {
         // this.level = readHandle.readUint16();
         this.exp = readHandle.readUint16();
         this.score = readHandle.readUint16();
+        this.remainAtkCoolPer = readHandle.readUint16();
+        this.chargingPer = readHandle.readUint16();
     }
 }
 
