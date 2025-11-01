3c5a161f24ed6e44cad55106c17a8ab018750226
2m8 <doyun7731@gmail.com>
2025-11-01 18:08:37 +0900

    entityData에 scale 값 추가

--- Datas/CharacterEntityDynamicData.js
+++ Datas/CharacterEntityDynamicData.js
@@ -6,22 +6,26 @@ class CharacterEntityDynamicData extends DamagableEntityDynamicData {
     constructor(character) {
         super(character);
         this.position = character?.position ?? new Vector();
+        this.scale = character?.scale ?? 1;
     }
 
     getFlexibleSize() {
         let size = super.getFlexibleSize();
         size += this.position.getFlexibleSize() + getBytesHeaderSize(); // position
+        size += 4; // scale
         return size;
     }
 
     onSerialize(writeHandle) {
         super.onSerialize(writeHandle);
         writeHandle.writeArrayBuffer(this.position.serialize());
+        writeHandle.writeUint32(this.scale);
     }
 
     onDeserialize(readHandle) {
         super.onDeserialize(readHandle);
         this.position = new Vector().deserialize(readHandle.readArrayBuffer());
+        this.scale = readHandle.readUint32();
     }
 }
 
--- Datas/StaticEntityStaticData.js
+++ Datas/StaticEntityStaticData.js
@@ -6,22 +6,26 @@ class StaticEntityStaticData extends EntityStaticData {
     constructor(entity) {
         super(entity);
         this.position = entity?.position ?? new Vector();
+        this.scale = entity?.scale ?? 1;
     }
 
     getFlexibleSize() {
         let size = super.getFlexibleSize();
         size += this.position.getFlexibleSize() + getBytesHeaderSize(); // position + header
+        size += 4; // scale
         return size;
     }
     
     onSerialize(writeHandle) {
         super.onSerialize(writeHandle);
         writeHandle.writeArrayBuffer(this.position.serialize());
+        writeHandle.writeUint32(this.scale);
     }
 
     onDeserialize(readHandle) {
         super.onDeserialize(readHandle);
         this.position = new Vector().deserialize(readHandle.readArrayBuffer());
+        this.scale = readHandle.readUint32();
     }
 }
 
