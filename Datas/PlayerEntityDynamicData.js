134fbe0a1d60626108a9ab640f8ebbd11243ce68
2m8 <doyun7731@gmail.com>
2025-11-02 21:16:11 +0900

    데이터에서 ui 정보 제거

--- Datas/PlayerEntityDynamicData.js
+++ Datas/PlayerEntityDynamicData.js
@@ -9,21 +9,12 @@ class PlayerEntityDynamicData extends CharacterEntityDynamicData {
 
         this.exp = player?.levelComponent?.xpAmount ?? 0;
         this.score = player?.score ?? 0;
-
-        const attackComponent = player?.attackComponent ?? undefined;
-        const remainAtkCoolRatio = (attackComponent?.getRemainAttackCooltimePer() ?? 0) * 100;
-        this.remainAtkCoolPer = Math.round(remainAtkCoolRatio);
-
-        const chargingRatio = (attackComponent?.getChargingPer() ?? 0) * 100;
-        this.chargingPer = Math.round(chargingRatio);
     }
 
     getFlexibleSize() {
         let size = super.getFlexibleSize();
         size += 2; // exp
         size += 2; // score
-        size += 2; // remainAtkCoolPer
-        size += 2; // chargingPer
         return size;
     }
 
@@ -31,16 +22,12 @@ class PlayerEntityDynamicData extends CharacterEntityDynamicData {
         super.onSerialize(writeHandle);
         writeHandle.writeUint16(this.exp);
         writeHandle.writeUint16(this.score);
-        writeHandle.writeUint16(this.remainAtkCoolPer);
-        writeHandle.writeUint16(this.chargingPer);
     }
 
     onDeserialize(readHandle) {
         super.onDeserialize(readHandle);
         this.exp = readHandle.readUint16();
         this.score = readHandle.readUint16();
-        this.remainAtkCoolPer = readHandle.readUint16();
-        this.chargingPer = readHandle.readUint16();
     }
 }
 
