f26e8cc25958be984de4aa88f174f69f2a3dd448
SEH00N <06psh0501@gmail.com>
2025-11-07 01:56:28 +0900

    [Balancing]

--- Configs/StatConfig.js
+++ Configs/StatConfig.js
@@ -13,7 +13,7 @@ const DefaultValue = {
     [Type.ATK_COOLTIME]: 2,
     [Type.POWER]: 15,
     [Type.MOVE_SPEED]: 4,
-    [Type.AUTO_HEAL]: 1.5
+    [Type.AUTO_HEAL]: 0.5
 };
 
 export const StatConfig = { Type, DefaultValue };
