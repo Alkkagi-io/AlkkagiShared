fc31dc1c4ba7a9aeceb5238e9900e6bf83cee9f7
2m8 <doyun7731@gmail.com>
2025-11-30 17:37:37 +0900

    enterWorldRequestPacket에서 모바일인지 넘겨주도록

--- Packets/C2S_EnterWorldRequestPacket.js
+++ Packets/C2S_EnterWorldRequestPacket.js
@@ -2,10 +2,11 @@ import { getFlexiableUTF8Size } from '../Modules/BufferHandle.js';
 import { Packet, EPacketID } from "./index.js";
 
 class C2S_EnterWorldRequestPacket extends Packet {
-    constructor(nickname) {
+    constructor(nickname, isMobile) {
         super();
 
         this.nickname = nickname;
+        this.isMobile = isMobile;
     }
 
     getPacketID() {
@@ -16,6 +17,7 @@ class C2S_EnterWorldRequestPacket extends Packet {
         let size = super.getFlexibleSize();
 
         size += getFlexiableUTF8Size(this.nickname); // utf8 str (max 4 bytes)
+        size += getUint8Size(this.isMobile);
 
         return size;
     }
@@ -24,12 +26,14 @@ class C2S_EnterWorldRequestPacket extends Packet {
         super.onSerialize(writeHandle);
 
         writeHandle.writeStringUTF8(this.nickname);
+        writeHandle.writeUint8(this.isMobile);
     }
 
     onDeserialize(readHandle) {
         super.onDeserialize(readHandle);
 
         this.nickname = readHandle.readStringUTF8();
+        this.isMobile = readHandle.readUint8();
     }
 }
 
